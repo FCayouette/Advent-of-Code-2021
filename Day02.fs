23bf7d6c9d12c86746f99351e9b48f0a71b5d9c3
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2021-12-02 23:46:21 -0500

    Day 02 F# Solution

---
+++ Day02.fs
@@ -0,0 +1,12 @@
+﻿open System.IO
+
+let ParseData (x:string) =
+    let data = x.Split ' '
+    data.[0].[0], System.Int32.Parse data.[1]
+
+[<EntryPoint>]
+let main argv =
+    let data = File.ReadAllLines argv.[0] |> Seq.map ParseData
+    let h,d,d2 = Seq.fold(fun (h,a,d) (inst, v) -> match inst with | 'u'->(h,a-v,d) | 'd'->(h,a+v,d) | _->(h+v, a, d+a*v)) (0,0,0) data
+    printfn "Part 1: %A \nPart 2: %A" (h*d) (h*d2) 
+    0 // return an integer exit code
