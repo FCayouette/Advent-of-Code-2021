4b2561100add729d83c4b9ecf5d4b2afdccee698
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2021-12-02 23:45:56 -0500

    Day 01 F# solution

---
+++ Day01.fs
@@ -0,0 +1,8 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let data = File.ReadAllLines(argv.[0]) |> Seq.map System.Int32.Parse
+    Seq.pairwise data |> Seq.filter(fun (a,b) -> a < b) |> Seq.length |> printfn "Part 1: %A"
+    Seq.windowed 4 data |> Seq.filter(fun a -> a.[0] < a.[3]) |> Seq.length |> printfn "Part 2: %A"
+    0
