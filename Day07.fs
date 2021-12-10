a9c27117d5c67b426d4511d219f2511946291144
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2021-12-10 18:43:15 -0500

    Day 07 F# Solution

---
+++ Day07.fs
@@ -0,0 +1,15 @@
+﻿open System.IO
+
+let LinDiff i lobsters =
+    Array.fold(fun t x -> t + abs(x-i)) 0L lobsters
+
+let QuadDiff i lobsters =
+    Array.fold(fun t x -> t + (abs(x-i) * (abs(x-i)+1L)/2L)) 0L lobsters
+
+[<EntryPoint>]
+let main argv =
+    let lobsters = File.ReadAllText(argv.[0]).Split(',') |> Array.map System.Int64.Parse
+    let pos = [|(Array.min lobsters) .. (Array.max lobsters)|]
+    Array.map(fun x -> LinDiff x lobsters) pos |> Array.min |> printfn "Part 1: %A"
+    Array.map(fun x -> QuadDiff x lobsters) pos |> Array.min |> printfn "Part 2: %A"
+    0
