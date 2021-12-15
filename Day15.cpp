c8dec60ccf53782fccbbcc6b868401a8418d80f4
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2021-12-15 00:50:08 -0500

    Day 15 Solution and Data

---
+++ Day15.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using Matrix = std::vector<std::vector<int>>;
+
+struct AstarVal
+{
+    size_t pos, cost, estimatedCost, total;
+    constexpr AstarVal(size_t p = 0, size_t c = 0, size_t ec = (size_t)-1) : pos(p), cost(c), estimatedCost(ec), total(c + ec) {}
+    AstarVal& operator=(const AstarVal&) = default;
+    constexpr bool operator>(const AstarVal& r) const { return total > r.total || (total == r.total && r.pos > pos); }
+};
+
+int FindPath(Matrix& matrix)
+{
+    int result = -matrix[0][0];
+
+    size_t rows = matrix.size(), cols = matrix.front().size();
+    size_t target = cols * rows - 1;
+    constexpr int computed = -1;
+
+    std::priority_queue<AstarVal, std::vector<AstarVal>, std::greater<AstarVal>> queue;
+    int part1 = -matrix[0][0], part2 = part1, min = 1;
+    queue.emplace(0, matrix[0][0], (cols + rows - 2) * min);
+    while (!queue.empty())
+    {
+        AstarVal val = queue.top();
+        queue.pop();
+        if (val.pos == target)
+            return (int)val.cost + result;
+        size_t y = val.pos / cols, x = val.pos % cols;
+        if (matrix[y][x] == computed)
+            continue;
+        matrix[y][x] = computed;
+
+        if (x > 0 && matrix[y][x - 1] != computed)
+            queue.emplace(val.pos - 1, matrix[y][x - 1] + val.cost, val.estimatedCost + min);
+        if (y > 0 && matrix[y - 1][x] != computed)
+            queue.emplace(val.pos - cols, matrix[y - 1][x] + val.cost, val.estimatedCost + min);
+        if (x + 1 < cols && matrix[y][x + 1] != computed)
+            queue.emplace(val.pos + 1, matrix[y][x + 1] + val.cost, val.estimatedCost - min);
+        if (y + 1 < rows && matrix[y + 1][x] != computed)
+            queue.emplace(val.pos + cols, matrix[y + 1][x] + val.cost, val.estimatedCost - min);
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << "Day15.exe inputfilename" << std::endl;
+        return -1;
+    }
+
+    std::ifstream in(argv[1], std::ios::in);
+    if (!in)
+    {
+        std::cout << "Could not open " << argv[1] << std::endl;
+        return -1;
+    }
+
+    Matrix matrix, expandedMatrix, fullMatrix;
+    std::string s;
+    while (in >> s)
+    {
+        std::vector<int> r;
+        for (char c : s)
+            r.push_back(c - '0');
+        matrix.push_back(r);
+        std::vector<int> extended = r;
+        for (int i = 1; i < 5; ++i)
+            for (int j : r)
+                extended.push_back(j + i <= 9 ? j + i : j + i - 9);
+        expandedMatrix.push_back(extended);
+    }
+
+    fullMatrix = expandedMatrix;
+    for (int i = 1; i < 5; ++i)
+        for (const auto& line : expandedMatrix)
+        {
+            std::vector<int> f;
+            for (int j : line)
+                f.push_back(j + i <= 9 ? j + i : j + i - 9);
+            fullMatrix.push_back(f);
+        }
+    
+    std::cout << "Part 1: " << FindPath(matrix) << "\nPart 2: " << FindPath(fullMatrix) << std::endl;
+    return 0;
+}
