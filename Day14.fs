075c6288d3e50e047cb0e56264df72e0aedc3554
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2022-01-03 23:35:13 -0500

    Day 14 F# Solution

---
+++ Day14.fs
@@ -0,0 +1,41 @@
+﻿open System.IO
+
+let GenerateTable (inst:string[]) =
+    Array.map(fun (s:string)->s.Replace(" -> ", "")) inst |> Array.fold(fun (table:Map<(char*char), char>) s -> table.Add((s.[0],s.[1]), s.[2])) (Map[])
+
+let InitialCount (init:string) =
+    let cArray = init.ToCharArray()
+    let counts = Array.countBy(id) cArray |> Array.fold(fun (map:Map<char, int64>) (x,y)-> map.Add(x,(int64)y)) (Map[])
+    let pairs = Array.pairwise cArray |> Array.countBy(id) |> Array.fold(fun (map:Map<(char * char), int64>) (x,count) -> map.Add(x, (int64)count)) (Map[])
+    (counts, pairs)
+
+let AddToCount (map:Map<char, int64>) (a, count) = 
+    if None <> map.TryFind(a) then map.Add(a, count + map.[a]) else map.Add(a, count)
+
+let AddToPairs (map:Map<char*char, int64>) (a,count) =
+    if None<>map.TryFind(a) then map.Add(a, count + map.[a]) else map.Add(a, count)
+
+let ProcessPair ((counts:Map<char, int64>), (pairs:Map<char*char, int64>), (table:Map<char*char, char>)) pair delta =
+    let c = table.[pair]
+    let newCounts = AddToCount counts (c, delta)
+    let newPairs = AddToPairs (AddToPairs pairs ((fst pair, c), delta)) ((c, snd pair), delta)
+    (newCounts, newPairs, table)
+
+let Polymerize ((counts:Map<char, int64>), (pairs:Map<char*char, int64>)) table =
+    let (newC, newP, newT) = Map.fold(ProcessPair) (counts, (Map[]), table) pairs
+    (newC, newP)
+
+let Count (map:Map<char, int64>) =
+    let (min, max) = Map.fold(fun (m, M) _ c -> (min m c, max M c)) (System.Int64.MaxValue,0L) map
+    max - min
+
+[<EntryPoint>]
+let main argv =
+    let raw = File.ReadAllLines(argv.[0])
+    let start = InitialCount raw.[0]
+    let table = Array.filter(fun (s:string) -> s.Contains("->")) raw |> GenerateTable
+    let (part1, _) = Array.fold(fun t _->Polymerize t table) start [|1..10|]
+    printfn "Part 1: %A" <| Count part1
+    let (part2, _) = Array.fold(fun t _ -> Polymerize t table) start [|1..40|]
+    printfn "Part 2: %A" <| Count part2
+    0
