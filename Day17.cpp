e00f2fbf1206fb048b63aa9c9a8663f2bf696155
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2022-01-07 14:35:08 -0500

    Simplified Day 17 Solution

--- Day17.cpp
+++ Day17.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <numeric>
-
-constexpr bool Within(int min, int val, int max)
-{
-    return min <= val && val <= max;
-}
 
 int main(int argc, char* argv[])
 {
@@ -26,7 +20,7 @@ int main(int argc, char* argv[])
     std::string s;
     in >> s;
     in >> s;
-    int minX, minY, maxX, maxY, maxPos = 0;
+    int minX, minY, maxX, maxY, mx = 0, part2 = 0;
     in >> s;
     s = s.substr(2, s.size()-3);
     auto p = s.find('.');
@@ -40,37 +34,25 @@ int main(int argc, char* argv[])
 
     auto Simulate = [minX, maxX, minY, maxY](int vx, int vy)
     {
-        int px = 0, py = 0, maxpy = 0;
-        bool hit = false;
-        while (!hit && py > minY && px <= maxX)
+        int px = 0, py = 0;
+        while (py >= minY && px <= maxX)
         {
+            if (px >= minX && py <= maxY) 
+                return true;
             px += vx;
             py += vy--;
-            maxpy = std::max(maxpy, py);
-            if (Within(minX, px, maxX) && Within(minY, py, maxY))
-                return maxpy;
             vx = std::max(0, vx - 1);
         }
-        return -1;
+        return false;
     };
 
-    int min = 0, maxVy = 0, part2 = 0;
-    while (min * (min + 1) / 2 < minX)
-        ++min;
-    
-    for (int i = 0; i < 100; ++i)
-        if (int result = Simulate(min, i); 
-            result > 0)
-        {
-            maxVy = i;
-            maxPos = std::max(maxPos, result);
-        }
-
-    for (int x = min; x <= maxX; ++x)
-        for (int y = minY; y <= maxVy; ++y)
-            if (Simulate(x, y) >= 0)
+    while (mx * (mx + 1) / 2 < minX)
+        ++mx;
+    for (int x = mx; x <= maxX; ++x)
+        for (int y = minY, mY = -minY; y < mY; ++y)
+            if (Simulate(x, y))
                 ++part2;
     
-    std::cout << "Part 1: " << maxPos << "\nPart 2: " << part2 << std::endl;
+    std::cout << "Part 1: " << minY*(minY+1)/2 << "\nPart 2: " << part2 << std::endl;
     return 0;
 }
